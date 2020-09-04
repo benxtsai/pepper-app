567f2a21ce120962d27d7752cbf2c496b4f2d277
Ben Tsai <29670876+itsbentsai@users.noreply.github.com>
2020-09-04 16:20:36 +0800

    Create a new say action when the robot focus is gained

--- app/src/main/java/com/pacificamericanschool/pepperapp/MainActivity.java
+++ app/src/main/java/com/pacificamericanschool/pepperapp/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.QiSDK;
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.SayBuilder;
 import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.aldebaran.qi.sdk.object.conversation.Say;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -36,6 +38,14 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
     @Override
     public void onRobotFocusGained(QiContext qiContext) {
         // The robot focus is gained.
+
+        // Create a new say action.
+        Say say = SayBuilder.with(qiContext) // Create the builder with the context.
+                .withText("Hello human!") // Set the text to say.
+                .build(); // Build the say action.
+
+        // Execute the action.
+        say.run();
     }
 
     @Override
